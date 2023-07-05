8099223ce62fea735f9fb554cceacfbcb41ae709
Matt Lavis <matthew.lavis@gmail.com>
2023-07-05 22:30:25 +0100

    Updates

--- classes/coverage_checker.py
+++ classes/coverage_checker.py
@@ -9,15 +9,30 @@ class CoverageChecker:
     def __init__(self):
         self.missing_codes = []
         self.get_files()
+        self.get_exceptions()
         self.get_codes()
-        # self.write_report()
+
+    def get_exceptions(self):
+        filename = os.path.join(self.coverage_report_folder, "exceptions.json")
+        with open(filename) as f:
+            self.exceptions = json.load(f)
+            a = 1
 
     def get_files(self):
         self.files = []
         self.folder = os.path.join(os.getcwd(), "resources", "json_strategic")
-        for file in glob.glob(os.path.join(self.folder, "*.json")):
-            print(file)
-            self.files.append(file)
+        self.coverage_report_folder = os.path.join(os.getcwd(), "resources", "coverage_report")
+
+        # specific_country = "pacific"
+        specific_country = ""
+
+        if specific_country != "":
+            for file in glob.glob(os.path.join(self.folder, "*.json")):
+                if specific_country in file:
+                    self.files.append(file)
+        else:
+            for file in glob.glob(os.path.join(self.folder, "*.json")):
+                self.files.append(file)
 
         self.files.sort()
 
@@ -30,7 +45,7 @@ class CoverageChecker:
         and validity_end_date > current_date
         and number_indents > 0
         and goods_nomenclature_item_id < '9800000000'
-        order by 1 
+        order by 1
         """
         d = Database()
         params = []
@@ -57,7 +72,7 @@ class CoverageChecker:
             a = 1
         a = 1
         f.close()
-        
+
         for code in self.codes:
             found = False
             for rule_set in rule_sets:
@@ -73,10 +88,30 @@ class CoverageChecker:
         a = 1
 
     def write_report(self):
-        filename = os.path.join(os.getcwd(), "resources", "report", "report.csv")
+        problem_countries = []
+        filename = os.path.join(self.coverage_report_folder, "coverage_report.csv")
         f = open(filename, "w")
         for missing_code in self.missing_codes:
             if missing_code["code"][0:2] != "93":
-                f.write(missing_code["file"] + ", ")
-                f.write(missing_code["code"] + "\n")
+                write_exception = True
+                for exception in self.exceptions:
+                    if write_exception:
+                        if exception in missing_code["file"]:
+                            for excepted_code in self.exceptions[exception]:
+                                if missing_code["code"].startswith(excepted_code):
+                                    write_exception = False
+                                    break
+                if write_exception:
+                    f.write(missing_code["file"] + ", ")
+                    f.write(missing_code["code"] + "\n")
+                    if missing_code["file"] not in problem_countries:
+                        problem_countries.append(missing_code["file"])
+
+        f.close()
+
+        filename = os.path.join(self.coverage_report_folder, "problem_countries.csv")
+        f = open(filename, "w")
+        for problem_country in problem_countries:
+            f.write(problem_country + "\n")
+
         f.close()
